d3e07116e4ed58080558512e9e65a1c71c6290ab
Lawrence <lawrencechan27@gmail.com>
2024-10-26 21:07:21 +1100

    Added ability to add new lists and change lists

--- src/tasks.js
+++ src/tasks.js
@@ -2,6 +2,7 @@ import "./styles.css";
 // import { List, Task, render, newTaskModal } from "./tasks.js";
 
 // LISTS
+let lists = [];
 class List {
     constructor(title) {
         this.title = title;
@@ -58,7 +59,7 @@ function render(obj) {
     newTaskDescInput.placeholder = "Task Desc";
     newTaskModal.appendChild(newTaskDescInput);
     let newTaskDueInput = document.createElement("input");
-    newTaskDueInput.placeholder = "Task Due";
+    newTaskDueInput.type = "date";
     newTaskModal.appendChild(newTaskDueInput);
     let newTaskSubmit = document.createElement("button");
     newTaskSubmit.textContent = "Add";
@@ -67,49 +68,106 @@ function render(obj) {
     container.appendChild(newTaskModal);
 
     // Add new task
-
     function addNewTask() {
-        newTaskModal.close();
-        list1.add(
-            new Task(
-                newTaskTitleInput.value,
-                newTaskDescInput.value,
-                newTaskDueInput.value
-            )
+        if (
+            newTaskTitleInput.value &&
+            newTaskDescInput.value &&
+            newTaskDueInput.value
+        ) {
+            newTaskModal.close();
+            lists[0].add(
+                new Task(
+                    newTaskTitleInput.value,
+                    newTaskDescInput.value,
+                    newTaskDueInput.value
+                )
+            );
+            render(lists[0]);
+        }
+    }
+
+    // New list button
+    let newListBtn = document.createElement("button");
+    newListBtn.textContent = "New List";
+    newListBtn.addEventListener("click", () => newListModal.showModal());
+    container.appendChild(newListBtn);
+
+    // New list modal
+    let newListModal = document.createElement("dialog");
+    newListModal.classList.add("new-task-modal");
+    let newListNameInput = document.createElement("input");
+    newListNameInput.placeholder = "List Name";
+    newListModal.appendChild(newListNameInput);
+    let newListSubmit = document.createElement("button");
+    newListSubmit.textContent = "Add";
+    newListSubmit.addEventListener("click", () => addNewList());
+    newListModal.appendChild(newListSubmit);
+    container.appendChild(newListModal);
+
+    // Add new list
+    function addNewList() {
+        if (newListNameInput.value) {
+            newListModal.close();
+            lists.push(new List(newListNameInput.value));
+            new List();
+            render(lists[lists.length - 1]);
+        }
+    }
+
+    // Change list button
+    let changeListBtn = document.createElement("button");
+    changeListBtn.textContent = "Change List";
+    changeListBtn.addEventListener("click", () => changeListModal.showModal());
+    container.appendChild(changeListBtn);
+
+    // Change list modal
+    let changeListModal = document.createElement("dialog");
+    changeListModal.classList.add("new-task-modal");
+    for (let obj of lists) {
+        let listButton = document.createElement("button");
+        listButton.textContent = obj.title;
+        listButton.addEventListener("click", () =>
+            changeList(lists.indexOf(obj))
         );
-        render(list1);
+        changeListModal.appendChild(listButton);
+    }
+    container.appendChild(changeListModal);
+
+    // Change list function
+    function changeList(listIndex) {
+        changeListModal.close();
+        render(lists[listIndex]);
     }
 
     // Tasks
-    for (let task of obj.tasks) {
-        let taskDiv = document.createElement("div");
-        taskDiv.classList.add("task");
+    if (obj.tasks) {
+        for (let task of obj.tasks) {
+            let taskDiv = document.createElement("div");
+            taskDiv.classList.add("task");
 
-        // Title
-        let title = document.createElement("h2");
-        title.classList.add("task-title");
-        title.textContent = task.title;
-        taskDiv.appendChild(title);
+            // Title
+            let title = document.createElement("h2");
+            title.classList.add("task-title");
+            title.textContent = task.title;
+            taskDiv.appendChild(title);
+            // Description
+            let desc = document.createElement("p");
+            desc.classList.add("task-desc");
+            desc.textContent = task.description;
+            taskDiv.appendChild(desc);
+            // Due
+            let due = document.createElement("p");
+            due.classList.add("task-due");
+            due.textContent = `Due: ${task.due}`;
+            taskDiv.appendChild(due);
+            // Complete
+            let complete = document.createElement("p");
+            complete.classList.add("task-complete");
+            complete.textContent = task.complete;
+            taskDiv.appendChild(complete);
 
-        // Description
-        let desc = document.createElement("p");
-        desc.classList.add("task-desc");
-        desc.textContent = task.description;
-        taskDiv.appendChild(desc);
-
-        // Due
-        let due = document.createElement("p");
-        due.classList.add("task-due");
-        due.textContent = `Due: ${task.due}`;
-        taskDiv.appendChild(due);
-
-        // Complete
-        let complete = document.createElement("p");
-        complete.classList.add("task-complete");
-        complete.textContent = task.complete;
-        taskDiv.appendChild(complete);
-
-        container.appendChild(taskDiv);
+            container.appendChild(taskDiv);
+        }
     }
 }
 
@@ -117,22 +175,12 @@ export function openAddTaskModal() {
     let newTaskTitle = prompt("Task Title");
     let newTaskDesc = prompt("Task Description");
     let newTaskDue = prompt("Task Due Date");
-    list1.add(new Task(newTaskTitle, newTaskDesc, newTaskDue));
-    render(".container", list1);
+    lists[0].add(new Task(newTaskTitle, newTaskDesc, newTaskDue));
+    render(".container", lists[0]);
 }
 
 // TESTING
 
-let list1 = new List("Tasks");
-let list2 = new List("Shopping");
+lists.push(new List("Tasks"));
 
-list1.add(new Task("Make list", "Create vanilla todo list", "Tomorrow"));
-list1.add(new Task("Bins", "Take the bins out", "Friday"));
-list2.add(new Task("Tennis Shoes", "", "Friday"));
-list2.add(new Task("Beer", "Pale Ales", "Saturday"));
-
-// list1.remove(list1.tasks[1]);
-// list1.tasks[0].toggleComplete();
-// console.log(list1.tasks);
-
-render(list1);
+render(lists[0]);
